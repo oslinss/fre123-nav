bffa247fae25544c97ac8effc39d5c30a7c08778
oslinss <2201390944@qq.com>
2025-03-28 12:19:30 +0800

    Update login.ts

--- fre123_nav_admin/src/routers/modules/login.ts
+++ fre123_nav_admin/src/routers/modules/login.ts
@@ -1,17 +1,17 @@
-import { RouteRecordRaw } from 'vue-router'
+// import { RouteRecordRaw } from 'vue-router'
 
-// 首页模块
-const loginRouter: Array<RouteRecordRaw> = [
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('@/views/login/index.vue'),
-    meta: {
-      keepAlive: false,
-      requiresAuth: false,
-      key: 'loginPage',
-    },
-  },
-]
+// // 首页模块
+// const loginRouter: Array<RouteRecordRaw> = [
+//   {
+//     path: '/login',
+//     name: 'login',
+//     component: () => import('@/views/login/index.vue'),
+//     meta: {
+//       keepAlive: false,
+//       requiresAuth: false,
+//       key: 'loginPage',
+//     },
+//   },
+// ]
 
-export default loginRouter
+// export default loginRouter
